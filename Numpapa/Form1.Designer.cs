5fd59c79d8fd44a9542fa92d2718b34261192278
Sitthiphone <108825240+SitthiphoneDev@users.noreply.github.com>
2023-06-26 16:37:25 +0700

    all

--- Numpapa/Form1.Designer.cs
+++ Numpapa/Form1.Designer.cs
@@ -30,24 +30,39 @@ namespace Numpapa
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt11 = new System.Windows.Forms.TextBox();
+            this.txt10 = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txt19 = new System.Windows.Forms.TextBox();
+            this.txt13 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.txthuk = new System.Windows.Forms.TextBox();
+            this.txtpern = new System.Windows.Forms.TextBox();
+            this.txtag = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Font = new System.Drawing.Font("Noto Sans Lao", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(13, 24);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
@@ -55,10 +70,32 @@ namespace Numpapa
             this.groupBox1.Size = new System.Drawing.Size(250, 175);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "ລາຍລະອຽດ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Noto Sans Lao", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(91, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 28);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "11-30 ແມັດກ້ອນ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Noto Sans Lao Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(91, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 28);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "0-10 ແມັດກ້ອນ";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.txt11);
+            this.groupBox2.Controls.Add(this.txt10);
             this.groupBox2.Location = new System.Drawing.Point(291, 24);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
@@ -66,129 +103,183 @@ namespace Numpapa
             this.groupBox2.Size = new System.Drawing.Size(250, 175);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Text = "ບໍລິມາດ";
+            // 
+            // txt11
+            // 
+            this.txt11.Location = new System.Drawing.Point(41, 97);
+            this.txt11.Name = "txt11";
+            this.txt11.Size = new System.Drawing.Size(175, 35);
+            this.txt11.TabIndex = 1;
+            // 
+            // txt10
+            // 
+            this.txt10.Location = new System.Drawing.Point(41, 47);
+            this.txt10.Name = "txt10";
+            this.txt10.Size = new System.Drawing.Size(175, 35);
+            this.txt10.TabIndex = 0;
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.label8);
             this.groupBox3.Location = new System.Drawing.Point(568, 24);
             this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox3.Size = new System.Drawing.Size(250, 175);
+            this.groupBox3.Size = new System.Drawing.Size(147, 175);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "groupBox3";
+            this.groupBox3.Text = "ລາຄາ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(28, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 28);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "1910 ກີບ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 28);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "1379 ກີບ";
             // 
             // groupBox4
             // 
-            this.groupBox4.Location = new System.Drawing.Point(843, 24);
+            this.groupBox4.Controls.Add(this.txt19);
+            this.groupBox4.Controls.Add(this.txt13);
+            this.groupBox4.Location = new System.Drawing.Point(748, 24);
             this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox4.Size = new System.Drawing.Size(250, 175);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "groupBox4";
+            this.groupBox4.Text = "ເງິນຄ່ານ້ຳ";
+            // 
+            // txt19
+            // 
+            this.txt19.Location = new System.Drawing.Point(22, 97);
+            this.txt19.Name = "txt19";
+            this.txt19.Size = new System.Drawing.Size(202, 35);
+            this.txt19.TabIndex = 3;
+            // 
+            // txt13
+            // 
+            this.txt13.Location = new System.Drawing.Point(22, 38);
+            this.txt13.Name = "txt13";
+            this.txt13.Size = new System.Drawing.Size(202, 35);
+            this.txt13.TabIndex = 2;
+            this.txt13.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Noto Sans Lao", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(45, 264);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(271, 165);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "ປຸ່ມຄິດໄລ່ນ້ຳປະປາ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(563, 240);
+            this.label1.Location = new System.Drawing.Point(645, 241);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 28);
+            this.label1.Size = new System.Drawing.Size(49, 28);
             this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.label1.Text = "ຄ່ານ້ຳ:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(563, 298);
+            this.label2.Location = new System.Drawing.Point(602, 299);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 28);
+            this.label2.Size = new System.Drawing.Size(103, 28);
             this.label2.TabIndex = 6;
-            this.label2.Text = "label2";
+            this.label2.Text = "ຄ່າບຳລຸງຮັກສາ:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(563, 350);
+            this.label3.Location = new System.Drawing.Point(620, 351);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 28);
+            this.label3.Size = new System.Drawing.Size(85, 28);
             this.label3.TabIndex = 7;
-            this.label3.Text = "label3";
+            this.label3.Text = "ຄ່ານ້ຳເປື້ອນ:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(563, 401);
+            this.label4.Location = new System.Drawing.Point(612, 399);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 28);
+            this.label4.Size = new System.Drawing.Size(93, 28);
             this.label4.TabIndex = 8;
-            this.label4.Text = "label4";
+            this.label4.Text = "ອາກອນ 7%:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(563, 465);
+            this.label5.Location = new System.Drawing.Point(534, 462);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 28);
+            this.label5.Size = new System.Drawing.Size(181, 28);
             this.label5.TabIndex = 9;
-            this.label5.Text = "label5";
+            this.label5.Text = "ລວມເງິນທີ່ຕ້ອງຈ່າຍທັງໝົດ:";
             // 
-            // textBox1
+            // txtnum
             // 
-            this.textBox1.Location = new System.Drawing.Point(653, 237);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 35);
-            this.textBox1.TabIndex = 10;
+            this.txtnum.Location = new System.Drawing.Point(735, 238);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(220, 35);
+            this.txtnum.TabIndex = 10;
+            this.txtnum.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // txthuk
             // 
-            this.textBox2.Location = new System.Drawing.Point(653, 291);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(220, 35);
-            this.textBox2.TabIndex = 11;
+            this.txthuk.Location = new System.Drawing.Point(735, 292);
+            this.txthuk.Name = "txthuk";
+            this.txthuk.Size = new System.Drawing.Size(220, 35);
+            this.txthuk.TabIndex = 11;
             // 
-            // textBox3
+            // txtpern
             // 
-            this.textBox3.Location = new System.Drawing.Point(653, 343);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(220, 35);
-            this.textBox3.TabIndex = 12;
+            this.txtpern.Location = new System.Drawing.Point(735, 344);
+            this.txtpern.Name = "txtpern";
+            this.txtpern.Size = new System.Drawing.Size(220, 35);
+            this.txtpern.TabIndex = 12;
             // 
-            // textBox4
+            // txtag
             // 
-            this.textBox4.Location = new System.Drawing.Point(653, 398);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(220, 35);
-            this.textBox4.TabIndex = 13;
+            this.txtag.Location = new System.Drawing.Point(735, 399);
+            this.txtag.Name = "txtag";
+            this.txtag.Size = new System.Drawing.Size(220, 35);
+            this.txtag.TabIndex = 13;
             // 
-            // textBox5
+            // txttotal
             // 
-            this.textBox5.Location = new System.Drawing.Point(653, 458);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(220, 35);
-            this.textBox5.TabIndex = 14;
+            this.txttotal.Location = new System.Drawing.Point(735, 459);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(220, 35);
+            this.txttotal.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1106, 619);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtag);
+            this.Controls.Add(this.txtpern);
+            this.Controls.Add(this.txthuk);
+            this.Controls.Add(this.txtnum);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -203,6 +294,15 @@ namespace Numpapa
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,11 +320,19 @@ namespace Numpapa
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.TextBox txthuk;
+        private System.Windows.Forms.TextBox txtpern;
+        private System.Windows.Forms.TextBox txtag;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt11;
+        private System.Windows.Forms.TextBox txt10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt19;
+        private System.Windows.Forms.TextBox txt13;
     }
 }
 
--- Numpapa/Form1.cs
+++ Numpapa/Form1.cs
@@ -12,9 +12,55 @@ namespace Numpapa
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txt10.Text);
+            int num2 = int.Parse(txt11.Text);
+
+            int sum1 = num1 * 1379;
+            int sum2 = num2 * 1910;
+
+            txt13.Text = sum1.ToString();
+            txt19.Text = sum2.ToString();
+
+            int num3 = sum1 + sum2;
+            txtnum.Text = num3.ToString();
+
+            int pern = int.Parse(txtpern.Text);
+            float huk = float.Parse(txthuk.Text);
+
+            float sum = num3 + pern + huk;
+            float tax = (float)((sum) * 0.07);
+
+            txtag.Text = tax.ToString();
+            float total_price = sum + tax;
+            txttotal.Text = total_price.ToString();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            txt13.Enabled = false;
+            txt19.Enabled = false;
+            txtnum.Enabled = false;
+            txtag.Enabled = false;
+            txttotal.Enabled = false;
         }
     }
 }
